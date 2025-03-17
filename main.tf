d86a4eacff24acbba4809d24aa76ffa9af9fdf3e
Craig Threadgold <craig.threadgold@versaterm.com>
2025-03-17 10:59:11 -0400

    update fmt

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
 terraform {
   required_version = ">= 1.11.2"
-  
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.18.0"
     }
-  
+
   }
 
   backend "s3" {
-    bucket         	   = "craig-refactor-tfstate"
-    key              	   = "state/terraform-github-actions.tfstate"
-    region         	   = "us-west-2"
-    encrypt        	   = true
+    bucket  = "craig-refactor-tfstate"
+    key     = "state/terraform-github-actions.tfstate"
+    region  = "us-west-2"
+    encrypt = true
     #use_lockfile = true
   }
 }
@@ -27,24 +27,24 @@ locals {
 }
 
 resource "aws_security_group" "msk_sg" {
-    for_each = var.security_groups
-    name        = each.key
-    vpc_id      = "vpc-039c29e549919a4f4"
+  for_each = var.security_groups
+  name     = each.key
+  vpc_id   = "vpc-039c29e549919a4f4"
 
-    ingress {
-        from_port   = each.value.from_port
-        to_port     = each.value.to_port
-        protocol    = each.value.protocol
-        #cidr_blocks = local.all_cidrs
-        cidr_blocks = concat(var.eks_cidr_block, var.private_cidr_block)
-    }
+  ingress {
+    from_port = each.value.from_port
+    to_port   = each.value.to_port
+    protocol  = each.value.protocol
+    #cidr_blocks = local.all_cidrs
+    cidr_blocks = concat(var.eks_cidr_block, var.private_cidr_block)
+  }
 
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = var.tags
+  tags = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -3,29 +3,29 @@ variable "tags" {
 }
 
 variable "security_groups" {
-    type = map(object({
-        from_port = number
-        to_port = number
-        protocol = optional(string, "tcp")
-    }))
-    default = {
-        test1 = {
-            from_port = 8080
-            to_port = 8080
-        }
-        test2 = {
-            from_port = 1111
-            to_port = 1111
-            protocol = "udp"
-        }
+  type = map(object({
+    from_port = number
+    to_port   = number
+    protocol  = optional(string, "tcp")
+  }))
+  default = {
+    test1 = {
+      from_port = 8080
+      to_port   = 8080
     }
-  
+    test2 = {
+      from_port = 1111
+      to_port   = 1111
+      protocol  = "udp"
+    }
+  }
+
 }
 
 variable "eks_cidr_block" {
   description = "The CIDR block for the first EKS subnet"
   type        = list(string)
-  
+
   default = ["10.0.0.0/19", "10.0.32.0/19", "10.0.64.0/27"]
 }
 
